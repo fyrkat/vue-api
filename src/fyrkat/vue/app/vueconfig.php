cd516a4815cd82b49f8f97b668b2a166a13a038e
Peder Bergebakken Sundt <pbsds@hotmail.com>
2019-04-06 22:13:11 +0200

    Implement VueConfig loader, add base_url to config.toml

--- src/fyrkat/vue/app/vueconfig.php
+++ src/fyrkat/vue/app/vueconfig.php
@@ -2,11 +2,20 @@
 
 namespace fyrkat\vue\app;
 
+use Yosymfony\Toml\Toml;
+
 class VueConfig {
+
+	private static $globalConfig = null;
 
 	public static function getConfig(): VueConfig
 	{
-		// todo implement with TOML
+		if ( null === static::$globalConfig ) {
+			$config_file = implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'config.toml']);
+			$config = Toml::ParseFile( $config_file, true );
+			static::$globalConfig = new VueConfig( $config );
+		}
+		return static::$globalConfig;
 	}
 
 	private $config;
@@ -25,12 +34,20 @@ class VueConfig {
 	}
 
 	public function getRootCollectionType(): string {
-		$result = $this->config->root_collection->path;
+		$result = $this->config->root_collection->type;
 		if ( !is_string( $result ) ) {
 			throw new \ConfigException( 'root_collection.type must be string' );
 		}
 		if ( preg_match( '/[^a-z]/', $result ) ) {
 			throw new \ConfigException( 'root_collection.type can only contain lowercase [a-z]' );
+		}
+		return $result;
+	}
+
+	public function getRootCollectionBaseUrl(): string {
+		$result = $this->config->root_collection->base_url;
+		if ( !is_string( $result ) ) {
+			throw new \ConfigException( 'root_collection.base_url must be string' );
 		}
 		return $result;
 	}
