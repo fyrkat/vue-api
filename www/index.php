37625d4fe4ef42e0d83e072cf4cb86ff4ba89dc4
Peder Bergebakken Sundt <pbsds@hotmail.com>
2019-04-06 20:39:27 +0200

    Require _autoload.php in index.php.
    Made during triplet-programming with @yorinad @aleksanw

--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+require implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'src', '_autoload.php']);
 
 header( 'Content-Type: text/plain', true, 404 );
 die( "404 Not Found\r\n" );
